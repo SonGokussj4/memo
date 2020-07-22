e2573b350b596b3b32dfe0462964a48af9f626a7
Verner Jan <jverner@evektor.cz>
2020-07-22 10:19:52 +0200

    Add Offers/Orders Views/Controllers/MenuLinks

---
+++ memo/Controllers/OrdersController.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using memo.Models;
+
+namespace memo.Controllers
+{
+    public class OrdersController : Controller
+    {
+        public OrdersController()
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
