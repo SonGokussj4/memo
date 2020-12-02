24a6b89274a14bd80f1cee39e3f914f9d741e05b
Verner Jan <jverner@evektor.cz>
2020-12-02 10:32:09 +0100

    Add hovering dropdowns to nav-menu

--- memo/wwwroot/js/site.js
+++ memo/wwwroot/js/site.js
@@ -32,6 +32,36 @@ var datepickerParameters = new Array({
 
 
 // ==========================================================================
+// BOOTSTRAP ADDITIONS - LIN-NAV ON HOVER
+// ==========================================================================
+const $dropdown = $(".dropdown");
+const $dropdownToggle = $(".dropdown-toggle");
+const $dropdownMenu = $(".dropdown-menu");
+const showClass = "show";
+
+$(window).on("load resize", function() {
+  if (this.matchMedia("(min-width: 768px)").matches) {
+    $dropdown.hover(
+      function() {
+        const $this = $(this);
+        $this.addClass(showClass);
+        $this.find($dropdownToggle).attr("aria-expanded", "true");
+        $this.find($dropdownMenu).addClass(showClass);
+      },
+      function() {
+        const $this = $(this);
+        $this.removeClass(showClass);
+        $this.find($dropdownToggle).attr("aria-expanded", "false");
+        $this.find($dropdownMenu).removeClass(showClass);
+      }
+    );
+  } else {
+    $dropdown.off("mouseenter mouseleave");
+  }
+});
+
+
+// ==========================================================================
 // INITIALIZE UPON PAGE LOAD
 // ==========================================================================
 $(document).ready(function () {
